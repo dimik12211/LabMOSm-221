5c33b40917e6338105240d0b5ed8265f3b17ffc9
dimak <dima.kuznetsov.2000@mail.ru>
2024-05-26 13:31:14 +0300

    Добавлен метод QueueFile для очереди чтения файлов

--- src/main/java/project/service/FileService.java
+++ src/main/java/project/service/FileService.java
@@ -6,6 +6,10 @@ import org.thymeleaf.util.DateUtils;
 
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -15,15 +19,15 @@ public class FileService {
     private Map<String, String> pathDownload = new HashMap<>();
 
     public String createZIP(MultipartFile[] filesThis) throws IOException {
-        List<File> files=new ArrayList<>();
-        for (MultipartFile m:filesThis) {
+        List<File> files = new ArrayList<>();
+        for (MultipartFile m : filesThis) {
             File file = new File("D:\\ProjectIntellijIDEA\\lastKurs\\LabServApplication_1\\src\\main\\resources\\filesZIP", m.getOriginalFilename());
             file.setWritable(true, false);
             m.transferTo(file);
             files.add(file);
         }
         String currentTimeUID = String.valueOf(System.currentTimeMillis());
-        File zip = new File("D:\\ProjectIntellijIDEA\\lastKurs\\LabServApplication_1\\src\\main\\resources\\filesZIP\\"+ currentTimeUID + ".zip");
+        File zip = new File("D:\\ProjectIntellijIDEA\\lastKurs\\LabServApplication_1\\src\\main\\resources\\filesZIP\\" + currentTimeUID + ".zip");
         zip.setWritable(true, false);
         zip.createNewFile();
         String fileFolders = zipFileFolders(files, zip);
@@ -35,11 +39,10 @@ public class FileService {
         byte[] buf = new byte[1024];
         ZipOutputStream out = null;
         try {
-            // ZipOutputStream class: Complete the compression of files or folders
             out = new ZipOutputStream(new FileOutputStream(zipfile));
             for (int i = 0; i < srcfile.size(); i++) {
                 FileInputStream in = new FileInputStream(srcfile.get(i));
-                String filePath="";
+                String filePath = "";
                 if (filePath == null)
                     filePath = "";
                 else
@@ -60,7 +63,23 @@ public class FileService {
         return "Ошибка";
     }
 
-    /*public String createRAR() {
+    public void QueueFile() {
+        File f = null;
+        final BlockingQueue<File> queue = new ArrayBlockingQueue<File>(1000);
+        for (File kid : f.listFiles()) {
+            queue.add(kid);
+        }
+        ExecutorService pool = Executors.newFixedThreadPool(5);
+        for (int i = 1; i <= 5; i++) {
+            Runnable r = new Runnable() {
+                public void run() {
+                    File workFile = null;
+                    while ((workFile = queue.poll()) != null) {
 
-    }*/
+                    }
+                }
+            };
+            pool.execute(r);
+        }
+    }
 }
